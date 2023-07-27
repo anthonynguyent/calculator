fedb408fcca09b1e37a60b8025275c4a68e8e92c
Anthony <anthonynguyenthanh@gmail.com>
2023-07-26 21:01:58 -0700

    Cleaned up code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Global variables and functions
 let firstNumber;
 let secondNumber;
 let operator;
@@ -29,75 +30,79 @@ document.addEventListener("DOMContentLoaded", function () {
     let waitingForNextNumber = false;
   
     function updateDisplay() {
-      const displayElement = document.querySelector(".display");
-      displayElement.textContent = displayValue;
+        const displayElement = document.querySelector(".display");
+        displayElement.textContent = displayValue;
     }
   
     function clearDisplay() {
-      displayValue = "0";
-      firstNumber = null;
-      operator = null;
-      waitingForNextNumber = false;
-      updateDisplay();
+        displayValue = "0";
+        firstNumber = null;
+        operator = null;
+        waitingForNextNumber = false;
+        updateDisplay();
     }
   
     function evaluate() {
-      if (!waitingForNextNumber && operator && firstNumber !== null && displayValue !== "") {
-        const secondNumber = parseFloat(displayValue);
-        const result = operate(firstNumber, operator, secondNumber);
-        if (isNaN(result) || result === Infinity) {
-          displayValue = "Error";
-        } else {
-          displayValue = roundNumber(result).toString();
+        if (!waitingForNextNumber && operator && firstNumber !== null && displayValue !== "") {
+            const secondNumber = parseFloat(displayValue);
+            const result = operate(firstNumber, operator, secondNumber);
+
+            if (isNaN(result) || result === Infinity) {
+                displayValue = "Error";
+            } 
+            else {
+                displayValue = roundNumber(result).toString();
+            }
+            
+            firstNumber = displayValue; // Store the result as the new first number
+            waitingForNextNumber = true;
+            updateDisplay();
         }
-        firstNumber = displayValue; // Store the result as the new first number
-        waitingForNextNumber = true;
-        updateDisplay();
-      }
     }
   
     function roundNumber(number) {
-      const precision = 10;
-      return Math.round(number * 10 ** precision) / 10 ** precision;
+        const precision = 10;
+        return Math.round(number * 10 ** precision) / 10 ** precision;
     }
   
     function appendToDisplay(value) {
-      if (displayValue === "Error") {
-        clearDisplay();
-      }
-  
-      if (waitingForNextNumber) {
-        displayValue = value;
-        waitingForNextNumber = false;
-      } else {
-        if (displayValue === "0" || displayValue === 0) {
-          displayValue = value;
-        } else {
-          displayValue += value;
+        if (displayValue === "Error") {
+            clearDisplay();
         }
-      }
-      updateDisplay();
+    
+        if (waitingForNextNumber) {
+            displayValue = value;
+            waitingForNextNumber = false;
+        } else {
+            if (displayValue === "0" || displayValue === 0) {
+                displayValue = value;
+            } 
+            else {
+                displayValue += value;
+            }
+        }
+        updateDisplay();
     }
   
     function operate(num1, operator, num2) {
-      num1 = parseFloat(num1);
-      num2 = parseFloat(num2);
-  
-      switch (operator) {
-        case "+":
-          return num1 + num2;
-        case "-":
-          return num1 - num2;
-        case "x":
-          return num1 * num2;
-        case "/":
-          if (num2 === 0) {
-            return NaN; // Division by zero error
-          }
-          return num1 / num2;
-        default:
-          return NaN; // Invalid operator
-      }
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+    
+        switch (operator) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "x":
+                return num1 * num2;
+            case "/":
+                if (num2 === 0) {
+                    return NaN; // Division by zero error
+                }
+                return num1 / num2;
+            default:
+                return NaN; // Invalid operator
+        }
     }
   
     const numberButtons = document.querySelectorAll(".number");
@@ -106,28 +111,47 @@ document.addEventListener("DOMContentLoaded", function () {
     const clearButton = document.querySelector(".clear");
   
     numberButtons.forEach((button) => {
-      button.addEventListener("click", function () {
-        appendToDisplay(button.dataset.value);
-      });
+        button.addEventListener("click", function () {
+            appendToDisplay(button.dataset.value);
+        });
     });
   
     operatorButtons.forEach((button) => {
-      button.addEventListener("click", function () {
+        button.addEventListener("click", function () {
         if (!waitingForNextNumber) {
-          if (firstNumber === null && displayValue !== "") {
-            firstNumber = displayValue;
-            operator = button.dataset.value;
-            displayValue = ""; // Clear the display for the next input
-          } else {
-            evaluate();
-            if (displayValue !== "Error") {
-              operator = button.dataset.value;
+            if (firstNumber === null && displayValue !== "") {
+                firstNumber = displayValue;
+                operator = button.dataset.value;
+                displayValue = ""; // Clear the display for the next input
+            } 
+            else {
+                evaluate();
+                if (displayValue !== "Error") {
+                    operator = button.dataset.value;
+                }
             }
-          }
         }
       });
     });
-  
-    equalButton.addEventListener("click", evaluate);
+    
+    // Issue with num operator num equals [operator num equals]
+    equalButton.addEventListener("click", function () {
+        if (operator && firstNumber !== null && displayValue !== "") {
+            const secondNumber = parseFloat(displayValue);
+            const result = operate(firstNumber, operator, secondNumber);
+
+            if (isNaN(result) || result === Infinity) {
+                displayValue = "Error";
+            } 
+            else {
+                displayValue = roundNumber(result).toString();
+            }
+
+            operator = null; // Reset the operator
+            firstNumber = null; // Reset the first number to allow new calculations
+            waitingForNextNumber = true;
+            updateDisplay();
+        }
+    });
     clearButton.addEventListener("click", clearDisplay);
 });  
