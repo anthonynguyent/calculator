e0c62110747f6080c440df067831554b5c4322fe
Anthony <anthonynguyenthanh@gmail.com>
2023-07-26 20:16:52 -0700

    Finished HTML, CSS, JS, and updated README

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # calculator
+
+This is a simple calculator web application built with HTML, CSS, and JavaScript.
+
+## Preview
+
+![Calculator Preview](./screenshots/calculator.png)
+
+## Demo
+
+You can view a live demo of the project [here](https://anthonynguyent.github.io/calculator/).
+
+## Technologies Used
+- HTML5
+- CSS3
+- Javascript (ES6)
+- Visual Studio Code
+
+## Features
+
+- Perform basic arithmetic operations: addition, subtraction, multiplication, and division.
+- Clear the display to start a new calculation.
+- Evaluate the result with the equal (=) button.
+- Responsive design for different screen sizes.
+
+## How to Use
+
+1. Clone this repository to your local machine.
+2. Open the `index.html` file in your web browser.
+
+## How It Works
+
+The calculator supports the following operations:
+
+- Addition: `+`
+- Subtraction: `-`
+- Multiplication: `x`
+- Division: `/`
+
+Simply click the number buttons to enter the digits of the calculation. Press an operator button to select the desired operation. Click the equal (=) button to evaluate the expression and display the result.
+
+## Example
+
+Suppose you want to calculate `5 + 3 * 2`. Here's how you would enter it into the calculator:
+
+1. Click the number button `5`.
+2. Click the addition operator button `+`.
+3. Click the number button `3`.
+4. Click the multiplication operator button `x`.
+5. Click the number button `2`.
+6. Click the equal button `=`.
+
+The calculator will show the result `16`.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- script.js
+++ script.js
@@ -1,28 +1,133 @@
+let firstNumber;
+let secondNumber;
+let operator;
+let displayValue = "0";
+
+function add(firstNumber, secondNumber){
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber){
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber){
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber){
+    if (secondNumber === 0) {
+        return "Cannot divide by zero";
+    }
+    return firstNumber / secondNumber;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    function add(firstNumber, secondNumber){
-        return firstNumber + secondNumber;
+    let displayValue = "0";
+    let firstNumber = null;
+    let operator = null;
+    let waitingForNextNumber = false;
+  
+    function updateDisplay() {
+      const displayElement = document.querySelector(".display");
+      displayElement.textContent = displayValue;
     }
-
-    function subtract(firstNumber, secondNumber){
-        return firstNumber - secondNumber;
+  
+    function clearDisplay() {
+      displayValue = "0";
+      firstNumber = null;
+      operator = null;
+      waitingForNextNumber = false;
+      updateDisplay();
     }
-
-    function multiply(firstNumber, secondNumber){
-        return firstNumber * secondNumber;
+  
+    function evaluate() {
+      if (!waitingForNextNumber && operator && firstNumber !== null && displayValue !== "") {
+        const secondNumber = parseFloat(displayValue);
+        const result = operate(firstNumber, operator, secondNumber);
+        if (isNaN(result) || result === Infinity) {
+          displayValue = "Error";
+        } else {
+          displayValue = roundNumber(result).toString();
+        }
+        firstNumber = displayValue; // Store the result as the new first number
+        waitingForNextNumber = true;
+        updateDisplay();
+      }
     }
-
-    function divide(firstNumber, secondNumber){
-        return Math.floor(firstNumber / secondNumber);
+  
+    function roundNumber(number) {
+      const precision = 10;
+      return Math.round(number * 10 ** precision) / 10 ** precision;
     }
-
-    function operate(firstNumber, operand, secondNumber){
-        if(operand === '+') return add(firstNumber, secondNumber);
-        if(operand === '-') return subtract(firstNumber, secondNumber);
-        if(operand === '*') return multiply(firstNumber, secondNumber);
-        if(operand === '/') return divide(firstNumber, secondNumber);
+  
+    function appendToDisplay(value) {
+      if (displayValue === "Error") {
+        clearDisplay();
+      }
+  
+      if (waitingForNextNumber) {
+        displayValue = value;
+        waitingForNextNumber = false;
+      } else {
+        if (displayValue === "0" || displayValue === 0) {
+          displayValue = value;
+        } else {
+          displayValue += value;
+        }
+      }
+      updateDisplay();
     }
-
-    let firstNumber;
-    let secondNumber;
-    let operand;
-});
+  
+    function operate(num1, operator, num2) {
+      num1 = parseFloat(num1);
+      num2 = parseFloat(num2);
+  
+      switch (operator) {
+        case "+":
+          return num1 + num2;
+        case "-":
+          return num1 - num2;
+        case "x":
+          return num1 * num2;
+        case "/":
+          if (num2 === 0) {
+            return NaN; // Division by zero error
+          }
+          return num1 / num2;
+        default:
+          return NaN; // Invalid operator
+      }
+    }
+  
+    const numberButtons = document.querySelectorAll(".number");
+    const operatorButtons = document.querySelectorAll(".operator");
+    const equalButton = document.querySelector(".equal");
+    const clearButton = document.querySelector(".clear");
+  
+    numberButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        appendToDisplay(button.dataset.value);
+      });
+    });
+  
+    operatorButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        if (!waitingForNextNumber) {
+          if (firstNumber === null && displayValue !== "") {
+            firstNumber = displayValue;
+            operator = button.dataset.value;
+            displayValue = ""; // Clear the display for the next input
+          } else {
+            evaluate();
+            if (displayValue !== "Error") {
+              operator = button.dataset.value;
+            }
+          }
+        }
+      });
+    });
+  
+    equalButton.addEventListener("click", evaluate);
+    clearButton.addEventListener("click", clearDisplay);
+});  
